9bf452bfd7b09a44eaf6e0160096f96e0398d3bb
Yan <yan.i.andrade@hotmail.com>
2019-08-18 16:24:25 -0300

    Terceiro Commit
    bootstrap simples, na home, juros simples e resultado juros simples

--- src/java/br/com/fatecpg/web/JurosSimplesServlet.java
+++ src/java/br/com/fatecpg/web/JurosSimplesServlet.java
@@ -37,10 +37,27 @@ public class JurosSimplesServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet JurosSimplesServlet</title>");            
+            out.println("<meta charset='utf-8'>");
+            out.println("<meta name='viewport'content='width=device-width, initial-scale=1, shrink-to-fit=no'>");
+            out.println("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>");
+            out.println("<title>Servlet JurosSimples</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet JurosSimplesServlet at " + request.getContextPath() + "</h1>");
+            out.println("<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>");
+            out.println("<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>");
+            out.println("<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>");
+            out.println("<div class='container-fluid'>");
+            out.println("<center>");
+            out.println("<br></br>");
+            out.println("<form action = 'resultado-simples.html' method='GET'>" + "<fieldset>"
+                    + "<p><h3>Capital Inicial:<h3><p><input type = 'number' name = 'capital'> <br>"
+                    + "<p><h3>Taxa:<h3></p> <input type='number' name='taxa'> <br>"
+                    + "<p><h3>Tempo:<h3></p><input type='number' name='tempo'> <br><br>"
+                    + "<button><h3>Enviar</h3></button>"
+                    + "</fieldset>" + "</form>");
+            
+            out.println("</center>");
+            out.println("</div>");
             out.println("</body>");
             out.println("</html>");
         }
---
+++ src/java/br/com/fatecpg/web/ResultadoSimplesServlet.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author LeuoO
+ */
+@WebServlet(name = "ResultadoSimplesServlet", urlPatterns = {"/resultado-simples.html"})
+public class ResultadoSimplesServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            double capital = Double.parseDouble(request.getParameter("capital"));
+            double taxa = Double.parseDouble(request.getParameter("taxa"));
+            double tempo = Double.parseDouble(request.getParameter("tempo"));
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<meta charset='utf-8'>");
+            out.println("<meta name='viewport'content='width=device-width, initial-scale=1, shrink-to-fit=no'>");
+            out.println("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>");
+            out.println("<title>Servlet ResultadoSimples</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>");
+            out.println("<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>");
+            out.println("<center>");
+            out.println("<br></br>");
+            out.println("<h2>Resultado do Juros Simples: " + capital * taxa * tempo / 100 + "</h2>");
+            out.println("<center>");
+            out.println("<br></br>");
+            out.println("<button type='button' class='btn btn-outline-primary'>");
+            out.println("<h3><a href='home.html'>Home</a></h3>");
+            out.println("</button>");
+    
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
